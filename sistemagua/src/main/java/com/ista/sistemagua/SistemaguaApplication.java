20d5102850c8e4f5ef9e0065c94b56334c37635f
Saquipay <cristian.saquipay.est@tecazuay.edu.ec>
2020-10-04 10:34:50 -0500

    Creación back end para sistema agua

---
+++ sistemagua/src/main/java/com/ista/sistemagua/SistemaguaApplication.java
@@ -0,0 +1,13 @@
+package com.ista.sistemagua;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaguaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaguaApplication.class, args);
+	}
+
+}
---
+++ sistemagua/src/test/java/com/ista/sistemagua/SistemaguaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ista.sistemagua;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaguaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

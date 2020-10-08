81be3fc45d5298ac198abae898e391e7c9dae035
Saquipay <cristian.saquipay.est@tecazuay.edu.ec>
2020-10-08 00:23:51 -0500

    Métodos buscar en Medidor y Consumidor

--- sistemagua/src/main/java/com/ista/sistemagua/Controller/ConsumidorController.java
+++ sistemagua/src/main/java/com/ista/sistemagua/Controller/ConsumidorController.java
@@ -2,11 +2,14 @@ package com.ista.sistemagua.Controller;
 
 import java.util.List;
 
+import com.ista.sistemagua.Exception.ResourceNotFoundException;
 import com.ista.sistemagua.Model.Consumidor;
 import com.ista.sistemagua.Repository.ConsumidorRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -18,5 +21,11 @@ public class ConsumidorController {
     public List <Consumidor> getAllConsumidores(){
         return consumidorRepository.findAll();
     }
-    
+    @GetMapping("/consumidor/{id}")
+	public ResponseEntity<Consumidor> getConsumidorById(@PathVariable(value = "id") String cedula)
+			throws ResourceNotFoundException {
+		Consumidor consumidor = consumidorRepository.findById(cedula)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + cedula));
+		return ResponseEntity.ok().body(consumidor);
+    }
 }
--- sistemagua/src/main/java/com/ista/sistemagua/Controller/MedidorController.java
+++ sistemagua/src/main/java/com/ista/sistemagua/Controller/MedidorController.java
@@ -2,12 +2,13 @@ package com.ista.sistemagua.Controller;
 
 import java.util.List;
 
+import com.ista.sistemagua.Exception.ResourceNotFoundException;
 import com.ista.sistemagua.Model.Medidor;
 import com.ista.sistemagua.Repository.MedidorRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 @RestController
 @CrossOrigin(origins = "*")
 @RequestMapping("/api/sgap")
@@ -20,5 +21,24 @@ public class MedidorController {
     public List<Medidor> getAllConsumidores() {
         return medidorRepository.findAll();
     }
+    @GetMapping("/medidor/{id}")
+	public ResponseEntity<Medidor> getMedidorById(@PathVariable(value = "id") String id)
+			throws ResourceNotFoundException {
+		Medidor medidor = medidorRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Medidor not found for this id :: " + id));
+		return ResponseEntity.ok().body(medidor);
+    }
+    @PutMapping("/medidor/{id}")
+	public ResponseEntity<Medidor> updateMedidor(@PathVariable(value = "id") String id,
+			@RequestBody Medidor medidorDetails) throws ResourceNotFoundException {
+		    Medidor medidor = medidorRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Medidor not found for this id :: " + id));
+
+        medidor.setId(medidorDetails.getId());
+        medidor.setFecha_instalacion(medidorDetails.getFecha_instalacion());
+        medidor.setMarca(medidorDetails.getMarca());
+		final Medidor updatedMedidor = medidorRepository.save(medidor);
+		return ResponseEntity.ok(updatedMedidor);
+	}
 
 }
